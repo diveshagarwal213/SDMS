7531558ecb7d95cbd9ee8f5f66614797e331a7f0
Divesh Aggarwal <diveshagarwal213@gmail.com>
2021-03-09 17:38:44 +0530

    login css up

--- conn.php
+++ conn.php
@@ -4,6 +4,3 @@ $hostname = "http://localhost/sdms";
 $conn = mysqli_connect("localhost","root","","sdms") or die("connection failed");
 
 ?>
-<script>
-    var Hostname = "http://localhost/sdms";
-</script>
--- login.php
+++ login.php
@@ -11,25 +11,76 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="css/common.css">
+    <link rel="stylesheet" href="css/login.css">
     <script src="jquery.js"></script>
+    <style>
+        #navDiv{
+            border-bottom: 3px solid #E66F2C;
+        }
+    </style>
 </head>
 <body>
-    <form id="check">
-    username<input type="text" id="inputname"> <br>
-    password <input type="password" id="inputpass">
-    <button id="login">Login</button></form>
-
-    <h1>insert new data</h1>
-    <form id="insert">
-        Fullname <input id="fullname" type="text">
-        username <input type="text" id="username">
-        email <input type="email" id="email">
-        password <input type="password" id="password">
-        <button id="submit">Submit</button>
-    </form>
-    <div>
-        <a href="main.php">Go Without login</a>
+    <!--Section-x same as quick.php --> 
+    <div id="navDiv">
+      <div id="navDiv_a">
+        <a id="sdmsLogo" href="index.php">SDMS</a>               
+      </div>
+      
+      <div id="navDiv_b">
+        <a href="quickSearch.php" >Quick search</a>
+        <div id="userDiv">
+          <?php
+            if (isset($_SESSION['username'])) {
+              echo "<a id = 'user' href = 'userprofile.php'>". $_SESSION['username']. "</a>";
+              if ($_SESSION['userimage'] !== "") {
+                echo "<img src = '" . $_SESSION['userimage']. "' alt = 'not found2'> <br>";
+              } else {
+                echo "<img src = 'images/userdata/profile.jpg'  alt = 'not found'> <br>";
+              }               
+            }else{
+              echo "<a id ='user' href = 'login.php'>Login</a>";
+              echo "<img src = '" . "images/userdata/profile.jpg". "' alt = 'not found'>";
+            }
+          ?>
+        </div>
+      </div>
     </div>
+    <!--Section-x End-->
+
+    <div id="first_div">
+        <div id="second_div">
+            <div id="loginDiv">
+                <h1>Login</h1>
+                <h3>New to Sdms? <button id="create" >Creat an account</button></h3>
+                <form id="check">
+                    <input type="text" id="inputname" placeholder="Username" autocomplete="off"> <br>
+                    <input type="password" id="inputpass" placeholder="Password">
+                    <br> <br>
+                    <button id="login">Login</button>
+                    <p> <button>Forget Password ?</button></p>
+                    <br>
+                    <a id="main_page" href="main.php">continue Without Login</a>
+                </form>
+            </div>
+
+            <div id="regDiv">
+                <h1>Register</h1>
+                <h3>Already have an Account? <button id="already">Login</button></h3>
+                <form id="insert">
+                    <input id="fullname" type="text" placeholder="Full Name" autocomplete="off"> <br>
+                    <input type="text" id="username" placeholder="Username"> <br>
+                    <input type="email" id="email" placeholder="Email"> <br>
+                    <input type="password" id="password" placeholder="password"> <br>
+                    <br>
+                    <button id="submit">Submit</button>
+                    <p>By signing up you agree to our <button>Terms of Use </button></p>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <?php include "footer.php" ?>
 
     <script>
         $(document).ready(function(){
@@ -86,8 +137,18 @@
                 });
             });
 
+            //hide login
+            $("#create").on('click', function () {
+                $("#loginDiv").slideUp(1000)
+            });
 
-        });
+            //show login
+            $("#already").on('click', function () {
+                $("#loginDiv").slideDown(1000)
+            });
+
+
+        });//ready
     </script>
 </body>
 </html>
